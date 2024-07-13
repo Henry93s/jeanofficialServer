9fb4e73b50800d257b5b480b79880cbfb8b295e6
henry0903 <gudrjsdn8825@naver.com>
2024-07-14 05:55:29 +0900

    Feat: 게시판 전체글, 내글만 보기 로직 구현 완료

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -5,14 +5,14 @@ const router = Router();
 const asyncHandler = require('../middlewares/async-handler');
 const postService = require('../services/postService');
 
-// 모든 글 리스트 읽기 (완료)
+// 전체 글 중 1 페이지 또는 특정 페이지의 글 리스트 가져오기 (완료)
 router.get('/getallposts/:nowpage', asyncHandler(async (req,res) => { 
     const {nowpage} = req.params;
     const result = await postService.getAllposts({nowpage});
     return res.status(200).json(result);
 }));
 
-// 내 글만 보기
+// 내 글 중 1 페이지 또는 특정 페이지의 글 리스트 가져오기 (완료)
 router.post('/getmyposts', asyncHandler(async (req,res) => { 
     const {email, nowpage} = req.body;
     const result = await postService.getMyposts({email, nowpage});
@@ -35,34 +35,14 @@ router.post('/write', asyncHandler(async (req, res) => {
     return res.status(200).json(result);
 }));
 
-
-// 글 수정 페이지 내용 요청
-router.post('/getput', asyncHandler(async (req, res) => {
-    const query = {
-        __id: req.params.__id
-    };
-    const post = await Post.findOne(query).populate('author');
-    if(post.author.email === req.user.email){
-        res.statusCode = 200;
-        res.json(post);
-        return;
-    } else { 
-        // 로그인은 되어 있으나 사용자가 작성한 글이 아닐 때
-        res.statusCode = 400;
-        res.json({edit:"notedit"});
-    }
-}));
-
-
-// 글 수정
+// 글 수정 (완료)
 router.post('/put', asyncHandler(async (req, res) => {
     const {email, title, content, nanoid} = req.body;
     const result = await postService.putPost({email, title, content, nanoid});
     return res.status(200).json(result);
 }));
 
-
-// 글 삭제
+// 글 삭제 (완료)
 router.delete('/del', asyncHandler(async (req, res) => {
     const {email, nanoid} = req.body;
     const result = await postService.delPost({email, nanoid});
--- services/postService.js
+++ services/postService.js
@@ -1,9 +1,9 @@
 const {User, Post} = require('../models');
 
 class PostService {
-    // 전체 글 보기 리스트 가져오기 (완료)
+    // 전체 글 중 1 페이지 또는 특정 페이지의 글 리스트 가져오기 (완료)
     async getAllposts({nowpage}){
-        const page = Number(nowpage === "-1" ? 1 : Number(nowpage));
+        const page = Number(nowpage);
         const perPage = 10; 
         const posts = await Post.find().sort(({createAt: -1})).skip(perPage * (page - 1))
         .limit(perPage).populate('author');
@@ -21,18 +21,21 @@ class PostService {
         return data;
     }
 
-    // 내 글만 보기 리스트 가져오기
-    async getMyposts({email, nowpage}){
-        const user = await User.find({email});
-        // pagination
-        // nowpage 가 없으면 1 을 사용하고 있으면 있는 값 사용 !!
-        const page = Number(nowpage === "-1" ? 1 : Number(nowpage));
+    // 내 글 중 1 페이지 또는 특정 페이지의 글 리스트 가져오기 (완료)
+    async getMyposts({email, nowpage}){ 
+        const author = await User.findOne({email});
+        if(!author) { 
+            const error = new Error();
+            Object.assign(error, {code: 400, message: "유저 정보를 가져오지 못했습니다. 다시 확인해주세요."});
+            throw error;
+        }
+        const page = Number(nowpage);
         const perPage = 10; 
         // 여기서 sort 는 몽구스 전용 sort !
-        const posts = await Post.find({author: user}).sort(({createAt: -1})).skip(perPage * (page - 1))
+        const posts = await Post.find({author: author}).sort(({createAt: -1})).skip(perPage * (page - 1))
         .limit(perPage).populate('author');
         // pupulate 를 추가하여 User 의 objectID 와 같은 데이터를 JOIN
-        const total = await Post.countDocuments({author: user});
+        const total = await Post.countDocuments({author: author});
         const totalPage = Math.ceil(total/perPage);
     
         const data = {
